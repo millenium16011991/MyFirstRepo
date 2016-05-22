eb10e469c43df42648c9501259d0971872f144f8
vitaliy.manov <manu1991@yandex.ru>
2016-05-22 17:20:06 +0300

    "решение задачи"

---
+++ MyPlayground1.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var sum1 = 3*((0+333)*334/2)
+var sum2 = 5*((0+199)*200/2)
+var sum = sum1 + sum2
+
+
+
+
+
+
+
+
+
